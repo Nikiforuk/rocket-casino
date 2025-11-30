0a26f1e26637505023fcad5e853b3ca67b56962d
Nikolay Nikiforuk <nnp.nikiforuk@gmail.com>
2025-11-30 20:36:06 +0200

    try again fix bag with routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import { StrictMode } from 'react';
 
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/rocket-casino/">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
