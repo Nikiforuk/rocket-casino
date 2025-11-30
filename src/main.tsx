ba79b483e36480eb2ae364b86932940ef778f401
Nikolay Nikiforuk <nnp.nikiforuk@gmail.com>
2025-11-30 20:28:41 +0200

    fix error with deploy

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/rocket-casino/">
+    <BrowserRouter basename="/signup">
       <App />
     </BrowserRouter>
   </StrictMode>,
