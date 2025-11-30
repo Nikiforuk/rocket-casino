a0b5032cc89833b93f75b74ca1b673aca9e41e6a
Nikolay Nikiforuk <nnp.nikiforuk@gmail.com>
2025-11-30 20:40:38 +0200

    Prepare for deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import { StrictMode } from 'react';
 
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/rocket-casino/">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 );
